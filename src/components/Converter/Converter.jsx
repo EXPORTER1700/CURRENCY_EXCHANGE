92953fc307e0bff35be5a650a98f57d2123667db
EXPORTER1700 <i.stetsenko1998@gmail.com>
2022-05-01 18:42:26 +0300

    bug fix

--- src/components/Converter/Converter.jsx
+++ src/components/Converter/Converter.jsx
@@ -19,15 +19,37 @@ const Converter = () => {
         const [toCurrency] = currencies.filter(currency => currency.cc === to)
         return +((fromCurrency.rate * count) / toCurrency.rate).toFixed(2)
     }
-    const toggleFromCurrency = (name) => {
+    const onChangeFromSelect = (name) => {
         setFromCurrency(prevState => ({...prevState, name}))
         setToCurrency(prevState => ({
             name: prevState.name,
             count: converter(name, toCurrency.name, fromCurrency.count)
         }))
     }
-    const toggleToCurrency = (name) => {
+    const onChangeToSelect = (name) => {
         setToCurrency({name, count: converter(fromCurrency.name, name, fromCurrency.count)})
+    }
+    const onChangeFromInput = (event) => {
+        if (event.target.value == Number(event.target.value)) {
+            setFromCurrency(prevState => ({
+                ...prevState, count: Number(event.target.value)
+            }))
+            setToCurrency(prevState => ({
+                name: prevState.name,
+                count: converter(fromCurrency.name, toCurrency.name, event.target.value)
+            }))
+        }
+    }
+    const onChangeToInput = (event) => {
+        if (event.target.value == Number(event.target.value)) {
+            setToCurrency(prevState => ({
+                ...prevState, count: Number(event.target.value)
+            }))
+            setFromCurrency(prevState => ({
+                name: prevState.name,
+                count: converter(toCurrency.name, fromCurrency.name, event.target.value)
+            }))
+        }
     }
     const revers = () => {
         setFromCurrency({...toCurrency})
@@ -37,46 +59,25 @@ const Converter = () => {
         <div className={classes.container}>
             <div className={classes.group}>
                 <p className={classes.description}>Отдаете</p>
-                <CurrenciesSelect toggle={toggleFromCurrency} currencies={currencies} defaultValue={fromCurrency.name}/>
+                <CurrenciesSelect toggle={onChangeFromSelect()} currencies={currencies}
+                                  defaultValue={fromCurrency.name}/>
                 <input
                     className={classes.input}
                     type={'text'}
                     value={fromCurrency.count}
-                    onChange={event => {
-                        if (event.target.value == Number(event.target.value)) {
-                            setFromCurrency(prevState => ({
-                                ...prevState, count: Number(event.target.value)
-                            }))
-                            setToCurrency(prevState => ({
-                                name: prevState.name,
-                                count: converter(fromCurrency.name, toCurrency.name, event.target.value)
-                            }))
-                        }
-                    }
-                    }/>
+                    onChange={onChangeFromInput}/>
             </div>
             <button className={classes.button} onClick={revers}>
                 <img src={arrows} width={'100%'} height={'100%'} alt=""/>
             </button>
             <div className={classes.group}>
                 <p className={classes.description}>Получаете</p>
-                <CurrenciesSelect toggle={toggleToCurrency} currencies={currencies} defaultValue={toCurrency.name}/>
+                <CurrenciesSelect toggle={onChangeToSelect()} currencies={currencies} defaultValue={toCurrency.name}/>
                 <input
                     type={'text'}
                     className={classes.input}
                     value={toCurrency.count}
-                    onChange={event => {
-                        if (event.target.value == Number(event.target.value)) {
-                            setToCurrency(prevState => ({
-                                ...prevState, count: Number(event.target.value)
-                            }))
-                            setFromCurrency(prevState => ({
-                                name: prevState.name,
-                                count: converter(toCurrency.name, fromCurrency.name, event.target.value)
-                            }))
-                        }
-                    }
-                    }/>
+                    onChange={onChangeToInput}/>
             </div>
         </div>
     );
